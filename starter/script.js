ce10045140e658eeeb12d62611d9846ba5c3272b
hugo pereira <Hugopereira8814@gmail.com>
2023-08-04 12:03:34 +0100

    done, next

--- starter/script.js
+++ starter/script.js
@@ -136,7 +136,7 @@ const restaurant = {
 
 //const arr = [1,2 ...[3,4]];
 
-const [a, b, ...others] = [1, 2, 3, 4, 5];
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
 // console.log(a, b, others);
 
 // const [pizza, , Risotto, ...otherfood] = [
